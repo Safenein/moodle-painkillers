29a76551ee596f4fcaec8c772bd80729eb1d7a2e
Safenein <safenein@protonmail.com>
2025-03-11 16:01:55 +0100

    fix(discord): make discord use cli arguments

--- src/moodle_painkillers/__main__.py
+++ src/moodle_painkillers/__main__.py
@@ -1,10 +1,12 @@
-from . import main
+from . import main, parse_args
 from .discord import send_discord_notification
 
+args = parse_args()
 try:
-    main()
+    main(args)
     message: str = "Sent presence status!"
 except Exception as e:
     message: str = str(e)
 
-send_discord_notification(message)
+if args.discord_webhook:
+    send_discord_notification(message, discord_webhook=args.discord_webhook)
